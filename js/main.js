02ec093b9363318cb98ea5c962fdd3ccb74ae25e
mckennaamurphyy <mckennaevamurphy@gmail.com>
2026-06-28 14:16:24 -0600

    Fix aria-expanded attribute toggle in hamburger menu

--- js/main.js
+++ js/main.js
@@ -5,11 +5,7 @@ hamburger.addEventListener("click", () => {
 
     nav.classList.toggle("nav-open");
 
-    const expand = 
-        hamburger.getAttribute("aria-expanded") === "true";
-
-    hamburger.setAttribute(
-        "aria-expand",
-        !expanded
+    const expanded = hamburger.getAttribute("aria-expanded") === "true";
+hamburger.setAttribute("aria-expanded", !expanded);
     );
 });
